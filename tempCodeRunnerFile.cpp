e0630a85f1a9804aa536cbff259f2c012914c117
SCKagura <allipreya1234@gmail.com>
2023-01-03 16:06:45 +0700

    lab11_2_completed

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,57 +1,22 @@
 #include<iostream>
+#include<fstream>
+#include<string>
 using namespace std;
 
-int main(){
-	int count[5] = {0,0,0,0,0};
-	int student=0,i=0; 
-		char  grade=' ' ;
-	cout << "Please input grade of each student (A-F) or input 0 to exit.\n";
-	do{
-		student++ ;
+int main (){
+	ifstream source;
+	ofstream dest;
+	source.open("cheerbook.txt") ;
+	dest.open("cheerbook_copy.txt");
 	
-		cout << "Student [" <<i+1<< "]: ";
-		cin >> grade;
-		
-		if(grade=='A') 
-			{
-			count[0]++ ;
-			}
-		else if(grade=='B')
-		{
-			count[1]++ ;
-		}
-		else if(grade=='C')
-		{
-			count[2]++ ;
-		}
-		else if(grade=='D') 
-		{
-			count[3]++ ;
-		}
-		else if(grade=='F')
-		{
-			count[4]++ ;
-		}
-		else if(grade !='0')
-		
-		{ 
-			cout<<"Wrong input. Please input again." ;
-			i-- ;
-		} 
-		
-	
-		i++ ;
-	}while(grade != '0');
-	
-	
-	
-	cout << "In total "<<student<<" students.";
-	cout << "A = " << count[0] <<", ";
-	cout << "B = " << count[1] <<", ";	
-	cout << "C = " << count[2] <<", ";	
-	cout << "D = " << count[3] <<", ";	
-	cout << "F = " << count[4] <<", ";	
-	
+	dest<<"\"-------------------- BOOM ---------------------\"" ;
+	string textline ;
+	while (getline(source,textline))
+	{
+	cout<<textline<<"\n" ;
+	}
+	dest<< "\"-------------------- HA!! ---------------------\""
+    source.close();
+    dest.close();
 	return 0;
-	
 }
