027fe8bad1fa2b51c031b15fbc0bb45263a1fcc2
SCKagura <allipreya1234@gmail.com>
2023-01-03 16:55:09 +0700

    lab11_3_completed

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,32 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<cstdlib>
+#include<cmath> 
+#include <iomanip>
+using namespace std ;
+
+int main() {
+    cout << setprecision(3);
+    ifstream source ;
+    ofstream dest  ;
+    source.open("score.txt") ;
+    dest.open("lnwza007.txt") ;
+    string math ;
+    double num=0,average=0,deviation=0,sum=0,sigma=0,current=0;
+    while(getline(source,math))
+    {
+        current=stod(math) ;
+        sum=sum+current;
+        sigma+=pow(current,2) ;
+        num++ ;
+    }
+    average=sum/num ;
+    deviation=sqrt(sigma/num-pow(average,2)) ;
+    cout<<"Number of data = "<<num<<"\n" ;
+    cout<<"Mean = " <<average<<"\n" ;
+    cout<<"Standard deviation = "<<deviation<<"\n" ;
+
+source.close() ;
+dest.close() ;
+}
